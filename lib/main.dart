2a6af29cef0f726ca7cd51a38422ca82890d8558
davg80 <dguillin80@gmail.com>
2023-11-29 15:45:00 +0100

    Display one Container and increment method

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:game_paper_clic/receipe_page.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -6,10 +7,31 @@ void main() {
 }
 
 class ResourceApp extends ChangeNotifier {
+  var woodColor = const Color.fromRGBO(150, 121, 105, 1);
   var wood = 0;
   var ironOre = 0;
   var copperOre = 0;
   var coal = 0;
+
+  void incrementWood() {
+    wood++;
+    notifyListeners();
+  }
+
+  void incrementIronOre() {
+    ironOre++;
+    notifyListeners();
+  }
+
+  void incrementCopperOre() {
+    copperOre++;
+    notifyListeners();
+  }
+
+  void incrementCoal() {
+    coal++;
+    notifyListeners();
+  }
 }
 
 class MainApp extends StatelessWidget {
@@ -39,11 +61,58 @@ class ResourcePage extends StatefulWidget {
 }
 
 class _ResourceState extends State<ResourcePage> {
+  void _incrementCounterWood() {
+    var appState = Provider.of<ResourceApp>(context, listen: false);
+    appState.incrementWood();
+  }
+
+  void _navigate() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const ReceipePage()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        leading: IconButton(
+          icon: const Icon(Icons.local_atm_outlined),
+          onPressed: () {
+            _navigate();
+          },
+        ),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Provider.of<ResourceApp>(context).woodColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                  child: Column(children: [
+                    const Text('Wood'),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          _incrementCounterWood();
+                        },
+                        child: Text(
+                            Provider.of<ResourceApp>(context).wood.toString()),
+                      ),
+                    )
+                  ]),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
---
+++ lib/receipe_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReceipePage extends StatefulWidget {
+  const ReceipePage({super.key});
+
+  @override
+  State<ReceipePage> createState() => _ReceipePageState();
+}
+
+class _ReceipePageState extends State<ReceipePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
